35b9cf63384a098153b124144dd792a8763d7e70
RizkyPratama05 <rizkypp428@gmail.com>
2025-04-23 07:43:13 +0700

    Add project files.

---
+++ FormTambahTransaksi.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TransaksiApp
+{
+    partial class FormTambahTransaksi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtTanggal1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jumlah";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 210);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Keterangan";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(35, 268);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dtTanggal1
+            // 
+            this.dtTanggal1.AutoSize = true;
+            this.dtTanggal1.Location = new System.Drawing.Point(104, 249);
+            this.dtTanggal1.Name = "dtTanggal1";
+            this.dtTanggal1.Size = new System.Drawing.Size(58, 16);
+            this.dtTanggal1.TabIndex = 9;
+            this.dtTanggal1.Text = "Tanggal";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pemasukan",
+            "Pengeluaran"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 317);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtTanggal1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 540);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Transaksi";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(314, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(780, 540);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Laporan";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(768, 513);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Simpan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // FormTambahTransaksi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 581);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormTambahTransaksi";
+            this.Text = "FormTambahTransaksi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label dtTanggal1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ FormTambahTransaksi.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TransaksiApp
+{
+    public partial class FormTambahTransaksi : Form
+    {
+        private int idEdit = -1;
+        private string connectionString = "Data Source=MSI\\RIZKYPP;Initial Catalog=A4;Integrated Security=True";
+
+        public FormTambahTransaksi()
+        {
+            InitializeComponent();
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+
+        private void FormTambahTransaksi_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("pemasukan");
+            comboBox1.Items.Add("pengeluaran");
+            comboBox1.SelectedIndex = 0;
+            TampilkanLaporan();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            decimal jumlah = Convert.ToDecimal(textBox2.Text);
+            string keterangan = textBox3.Text;
+            DateTime tanggal = dateTimePicker1.Value;
+            string tipe = comboBox1.SelectedItem.ToString();
+
+            if (string.IsNullOrWhiteSpace(nama) || jumlah <= 0)
+            {
+                MessageBox.Show("Nama dan jumlah harus diisi dengan benar.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                if (idEdit == -1)
+                {
+                    string insertKategori = "INSERT INTO kategori (nama_kategori, tipe) VALUES (@nama, @tipe); SELECT SCOPE_IDENTITY();";
+                    SqlCommand cmdKategori = new SqlCommand(insertKategori, conn);
+                    cmdKategori.Parameters.AddWithValue("@nama", nama);
+                    cmdKategori.Parameters.AddWithValue("@tipe", tipe);
+                    int idKategori = Convert.ToInt32(cmdKategori.ExecuteScalar());
+
+                    string insertTransaksi = "INSERT INTO transaksi (id_kategori, jumlah, tanggal, keterangan) VALUES (@id_kategori, @jumlah, @tanggal, @keterangan)";
+                    SqlCommand cmdTransaksi = new SqlCommand(insertTransaksi, conn);
+                    cmdTransaksi.Parameters.AddWithValue("@id_kategori", idKategori);
+                    cmdTransaksi.Parameters.AddWithValue("@jumlah", jumlah);
+                    cmdTransaksi.Parameters.AddWithValue("@tanggal", tanggal);
+                    cmdTransaksi.Parameters.AddWithValue("@keterangan", keterangan);
+                    cmdTransaksi.ExecuteNonQuery();
+
+                    MessageBox.Show("Transaksi berhasil ditambahkan.");
+                }
+                else
+                {
+                    string updateTransaksi = "UPDATE transaksi SET jumlah=@jumlah, tanggal=@tanggal, keterangan=@keterangan WHERE id_transaksi=@id";
+                    SqlCommand cmdUpdate = new SqlCommand(updateTransaksi, conn);
+                    cmdUpdate.Parameters.AddWithValue("@jumlah", jumlah);
+                    cmdUpdate.Parameters.AddWithValue("@tanggal", tanggal);
+                    cmdUpdate.Parameters.AddWithValue("@keterangan", keterangan);
+                    cmdUpdate.Parameters.AddWithValue("@id", idEdit);
+                    cmdUpdate.ExecuteNonQuery();
+
+                    MessageBox.Show("Transaksi berhasil diupdate.");
+                    idEdit = -1;
+                }
+            }
+
+            ClearForm();
+            TampilkanLaporan();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                
+                if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+                string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+                if (columnName != "Update" && columnName != "Delete") return;
+
+                
+                var idValue = dataGridView1.Rows[e.RowIndex].Cells["id_transaksi"].Value;
+                if (idValue == DBNull.Value || idValue == null || idValue.ToString() == "") return;
+
+                int idTransaksi = Convert.ToInt32(idValue);
+
+                if (columnName == "Update")
+                {
+                    
+                    textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["nama_kategori"].Value.ToString();
+                    comboBox1.SelectedItem = dataGridView1.Rows[e.RowIndex].Cells["tipe"].Value.ToString();
+                    textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["jumlah"].Value.ToString();
+                    textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["keterangan"].Value.ToString();
+                    dateTimePicker1.Value = Convert.ToDateTime(dataGridView1.Rows[e.RowIndex].Cells["tanggal"].Value);
+                    idEdit = idTransaksi;
+                }
+                else if (columnName == "Delete")
+                {
+                    var confirm = MessageBox.Show("Yakin ingin menghapus transaksi ini?", "Konfirmasi", MessageBoxButtons.YesNo);
+                    if (confirm == DialogResult.Yes)
+                    {
+                        using (SqlConnection conn = new SqlConnection(connectionString))
+                        {
+                            conn.Open();
+                            SqlCommand cmd = new SqlCommand("DELETE FROM transaksi WHERE id_transaksi = @id", conn);
+                            cmd.Parameters.AddWithValue("@id", idTransaksi);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Transaksi berhasil dihapus.");
+                            TampilkanLaporan();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}\n\nDetail: {ex.StackTrace}");
+            }
+        }
+
+        private void TampilkanLaporan()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT id_transaksi, nama_kategori, tipe, jumlah, tanggal, keterangan FROM view_transaksi_lengkap ORDER BY tanggal DESC";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    
+                    decimal totalPemasukan = 0, totalPengeluaran = 0;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        if (row["tipe"].ToString() == "pemasukan")
+                            totalPemasukan += Convert.ToDecimal(row["jumlah"]);
+                        else if (row["tipe"].ToString() == "pengeluaran")
+                            totalPengeluaran += Convert.ToDecimal(row["jumlah"]);
+                    }
+
+                    
+                    DataRow totalRow = dt.NewRow();
+                    totalRow["nama_kategori"] = "TOTAL";
+                    totalRow["tipe"] = "";
+                    totalRow["jumlah"] = DBNull.Value;
+                    totalRow["keterangan"] = $"Pemasukan: Rp{totalPemasukan:N0} | Pengeluaran: Rp{totalPengeluaran:N0} | Saldo: Rp{(totalPemasukan - totalPengeluaran):N0}";
+                    dt.Rows.Add(totalRow);
+
+                    
+                    dataGridView1.DataSource = null;
+                    dataGridView1.Columns.Clear();
+                    dataGridView1.AutoGenerateColumns = false;
+
+                    
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "id_transaksi",
+                        HeaderText = "ID",
+                        Name = "id_transaksi",
+                        Visible = false 
+                    });
+
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "nama_kategori",
+                        HeaderText = "Kategori",
+                        Name = "nama_kategori"
+                    });
+
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "tipe",
+                        HeaderText = "Jenis",
+                        Name = "tipe"
+                    });
+
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "jumlah",
+                        HeaderText = "Jumlah",
+                        Name = "jumlah",
+                        DefaultCellStyle = new DataGridViewCellStyle() { Format = "N0" }
+                    });
+
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "tanggal",
+                        HeaderText = "Tanggal",
+                        Name = "tanggal",
+                        DefaultCellStyle = new DataGridViewCellStyle() { Format = "dd/MM/yyyy" }
+                    });
+
+                    dataGridView1.Columns.Add(new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = "keterangan",
+                        HeaderText = "Keterangan",
+                        Name = "keterangan"
+                    });
+
+                   
+                    DataGridViewButtonColumn btnUpdate = new DataGridViewButtonColumn()
+                    {
+                        HeaderText = "Aksi",
+                        Name = "Update",
+                        Text = "Update",
+                        UseColumnTextForButtonValue = true,
+                        FlatStyle = FlatStyle.Flat,
+                        DefaultCellStyle = new DataGridViewCellStyle()
+                        {
+                            BackColor = Color.LightBlue,
+                            ForeColor = Color.Black
+                        }
+                    };
+
+                    DataGridViewButtonColumn btnDelete = new DataGridViewButtonColumn()
+                    {
+                        Name = "Delete",
+                        Text = "Delete",
+                        UseColumnTextForButtonValue = true,
+                        FlatStyle = FlatStyle.Flat,
+                        DefaultCellStyle = new DataGridViewCellStyle()
+                        {
+                            BackColor = Color.LightCoral,
+                            ForeColor = Color.Black
+                        }
+                    };
+
+                    dataGridView1.Columns.Add(btnUpdate);
+                    dataGridView1.Columns.Add(btnDelete);
+
+                    
+                    dataGridView1.DataSource = dt;
+
+                    
+                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 9, FontStyle.Bold);
+                    dataGridView1.EnableHeadersVisualStyles = false;
+                    dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.LightGray;
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridView1.RowHeadersVisible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saat menampilkan laporan: {ex.Message}");
+            }
+        }
+
+        private void ClearForm()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            comboBox1.SelectedIndex = 0;
+            dateTimePicker1.Value = DateTime.Now;
+            idEdit = -1;
+        }
+    }
+}
